df530fc8da2e81f70810b05303a2fdb82186f625
FP Wirth <56011287+fpwirth@users.noreply.github.com>
2019-10-26 19:10:34 -0500

    Updated main.py files(pybank and pyroll) with comments added

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -1,12 +1,16 @@
+#Import modules needed for working with CSV files
 import os
 import csv
 
+#Create lists for data
 date = []
 pl = []
 change = [0]
 
+#Set path for raw data
 pybankpath = os.path.join('Resources', 'budget_data.csv')
 
+#Read CSV raw data file, exclude header row and put data in lists
 with open(pybankpath, newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     pybankheader = next(reader)
@@ -14,26 +18,33 @@ with open(pybankpath, newline='') as csvfile:
         date.append(row[0])
         pl.append(int(row[1])) 
 
+#Determine number of months involved in analysis and total profits
 totalmonths = len(date)
 totalprofit = sum(pl)
 
+#Determine average monthly change in profits/losses and create monthly change list
 tchange = [x-y for x, y in zip(pl[1:], pl)]
 avgchange = float('{0:.2f}'.format(sum(tchange)/len(tchange)))
 change.extend(tchange)
 
+#Create list of date, profit/loss, change
 pybankdata = zip(date, pl, change)
 pybankdata = set(pybankdata)
 
+#Determine the max and min change in profit/loss based on all months
 maxchange = max(pybankdata, key=lambda x: x[2])
 minchange = min(pybankdata, key=lambda x: x[2])
 
+#Set path for financial analysis output
 outpybankpath = os.path.join('Resources', 'financial _analysis.csv')
 
+#Create CSV file containing financial analysis output
 with open(outpybankpath, 'w', newline='') as outfile:
     csvwriter = csv.writer(outfile, delimiter=',')
     csvwriter.writerow(['Total Months', 'Total', 'Average Change', 'Greatest Increase Date', 'Greatest Increase', 'Greatest Decrease Date', 'Greatest Decrease'])
     csvwriter.writerow([totalmonths, totalprofit, avgchange, maxchange[0], maxchange[2], minchange[0], minchange[2]])
 
+#Print financial analysis to terminal
 print('')
 print('Financial Analysis')
 print('----------------------------------------------------')
--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -1,12 +1,16 @@
+#Import modules needed for working with CSV files
 import os
 import csv
 
+#Create lists for data
 voter_id = []
 county = []
 candidate = []
 
+#Set path for raw data
 pypollpath = os.path.join('resources', 'election_data.csv')
 
+#Read CSV raw data file, exclude header row and put data in lists
 with open(pypollpath, newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     pypollheader = next(reader)
@@ -14,14 +18,18 @@ with open(pypollpath, newline='') as csvfile:
         voter_id.append(row[0])
         county.append(row[1])
         candidate.append(row[2])
-    
+
+#Determine number of votes overall, for each candidate and percentage won for each candidate 
 totalvotes = len(voter_id)
 candidatevotes = [(person,candidate.count(person),(candidate.count(person)/totalvotes)*100) for person in set(candidate)]
 
+#Determine winner of election based on popular vote
 winner = max(candidatevotes, key=lambda x: x[1])
 
+#Set path for election results output
 outpypollpath = os.path.join('resources', 'election_results.csv')
 
+#Create CSV file containing election results output
 with open(outpypollpath, 'w', newline='') as outfile:
     csvwrite =csv.writer(outfile, delimiter=',')
     csvwrite.writerow(['Total Votes', totalvotes, ''])
@@ -29,15 +37,16 @@ with open(outpypollpath, 'w', newline='') as outfile:
         csvwrite.writerow(row)
     csvwrite.writerow(['Winner', winner[0], ''])
 
+#Print election results to terminal
 print('')
 print('Election Results')
 print('----------------------------------------------------')
 print(f'Total Votes: {totalvotes}')
 print('----------------------------------------------------')
-print(f'{candidatevotes[3][0]}: {candidatevotes[3][2]:.3f}% ({candidatevotes[3][1]})')
+print(f'{candidatevotes[0][0]}: {candidatevotes[0][2]:.3f}% ({candidatevotes[0][1]})')
 print(f'{candidatevotes[1][0]}: {candidatevotes[1][2]:.3f}% ({candidatevotes[1][1]})')
 print(f'{candidatevotes[2][0]}: {candidatevotes[2][2]:.3f}% ({candidatevotes[2][1]})')
-print(f'{candidatevotes[0][0]}: {candidatevotes[0][2]:.3f}% ({candidatevotes[0][1]})')
-print('----------------------------------------------------'*2)
+print(f'{candidatevotes[3][0]}: {candidatevotes[3][2]:.3f}% ({candidatevotes[3][1]})')
+print('----------------------------------------------------')
 print(f'Winner: {winner[0]}')
 print('----------------------------------------------------')
